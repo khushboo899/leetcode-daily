eea169869534b6cc3d691f6a5a906d42ba197c6b
khushboo899 <guptakhushboo538@gmail.com>
2026-07-15 23:57:20 +0530

    Time: 4 ms (39.33%), Space: 54.2 MB (93.12%) - LeetHub

---
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            for(int x:matrix[i]){
+            ans[i] += x;
+            }
+        }
+        return ans;
+    }
+};
+
