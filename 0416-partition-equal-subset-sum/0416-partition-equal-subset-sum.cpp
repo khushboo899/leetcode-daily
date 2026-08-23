3b3591eacc4ecf5c3dbae88be228befe3fdbe42a
khushboo899 <guptakhushboo538@gmail.com>
2026-08-23 17:40:51 +0530

    Time: 86 ms (76.81%), Space: 13.3 MB (98.44%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        
+        // Odd sum cannot be split equally
+        if (total % 2 != 0) return false;
+        
+        int target = total / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true; // base case
+        
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
